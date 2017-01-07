4f2497b4067ca5aa223398fc50b57a620e88a41d
Gogs <gogitservice@gmail.com>
2017-01-07 17:16:35 +0800

    auth

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func connectRedis() {
 			panic(err)
 		}
 
-		_, err2 := redis_conn.Write([]byte("AUTH demo"))
+		_, err2 := redis_conn.Write([]byte("AUTH demo\r\nSELECT 0\r\n"))
 		if err2 != nil {
 			panic(err2)
 		}

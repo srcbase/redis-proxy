24bc474c20f2c9af6311530f20e8db7cb3286e79
Gogs <gogitservice@gmail.com>
2017-01-15 23:46:55 +0800

    consistent hash helper

--- lib/helper/consistent_hash.go
+++ lib/helper/consistent_hash.go
@@ -1,1 +1,20 @@
 package helper
+
+import (
+	"strings"
+)
+
+func Mhash(key string) int64 {
+	hash_string := Substr(Md5(key), 0, 8)
+
+	var seed int64
+	seed = 31
+	var hash int64
+	hash = 0
+	hash_arr := strings.Split(hash_string, "")
+	for _, val := range hash_arr {
+		hash = seed*hash + int64([]byte(val)[0])
+	}
+
+	return hash & 0x7FFFFFFF
+}

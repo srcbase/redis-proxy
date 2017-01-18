66cbc8b3d46e3708613784ed74b3a5a507794791
罗晓俊 <luoxiaojun@luoxiaojundeMac-mini.local>
2017-01-18 10:47:15 +0800

    redis sharding optimize

--- lib/helper/consistent_hash.go
+++ lib/helper/consistent_hash.go
@@ -4,7 +4,18 @@ import (
 	"strings"
 )
 
+var cache map[string]int64
+
+func init() {
+	cache = make(map[string]int64)
+}
+
 func Mhash(key string) int64 {
+	cache_hash := getCache(key)
+	if cache_hash != 0 {
+		return cache_hash
+	}
+
 	hash_string := Substr(Md5(key), 0, 8)
 
 	var seed int64
@@ -16,5 +27,21 @@ func Mhash(key string) int64 {
 		hash = seed*hash + int64([]byte(val)[0])
 	}
 
-	return hash & 0x7FFFFFFF
+	result := hash & 0x7FFFFFFF
+
+	setCache(key, result)
+
+	return result
+}
+
+func getCache(key string) int64 {
+	if hash, ok := cache[key]; ok {
+		return hash
+	}
+
+	return 0
+}
+
+func setCache(key string, hash int64) {
+	cache[key] = hash
 }
